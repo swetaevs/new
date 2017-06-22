91e44a49e2bd85d0a1c32a7a04ee1aab61e1f932
sweta.kumari <sweta.kumari@Evalueserve.com>
2017-06-22 16:34:15 +0530

    new method to test

--- test/src/pkg/login.java
+++ test/src/pkg/login.java
@@ -5,6 +5,15 @@ public class login
 public static void main(String a[])
 {
 	System.out.println("test git ");
+	System.out.println("new test ");
+	login l=new login();
+	l.me();
+}
+
+public static void me() 
+{
+	System.out.println("test method ");
+	
 }
 	
 }
